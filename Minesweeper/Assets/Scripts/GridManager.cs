92177622bf494a281e2dbe77e4bdca4e84947ca2
이강혁 <2020061676@hanyang.ac.kr>
2020-07-26 10:03:47 +0900

    message

---
+++ Minesweeper/Assets/Scripts/GridManager.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GridManager : MonoBehaviour
+{
+
+    public GameObject CloneBlock;
+
+    public int WidthBlock = 10;
+    public int HeightBlock = 10;
+
+    public Tile[,] ElementArray;
+
+    void Awake()
+    {
+        ElementArray = new Tile[WidthBlock, HeightBlock];
+    }
+
+    Tile CloneTile(int p_x, int p_y)   //타일 복제
+    {
+        GameObject CopyObj = GameObject.Instantiate(CloneBlock.gameObject);
+        CopyObj.transform.SetParent(this.transform);
+
+        Vector2 temppos = new Vector2(p_x, p_y);
+        CopyObj.transform.localPosition = temppos;
+        CopyObj.name = "CloneBlock_" + p_x.ToString() + "_" + p_y.ToString();
+
+        return CopyObj.GetComponent<Tile>();
+    }
+
+    void TileGenarator()   //타일 생성
+    {
+        for (int yy=0; yy< HeightBlock; ++yy)
+        {
+            for (int xx=0; xx< WidthBlock; ++xx)
+            {
+                ElementArray[xx, yy] = CloneTile(xx, yy);
+            }
+        }
+    }
+
+    bool GetMineAt(int p_x, int p_y)   //지뢰인지 확인
+    {
+        if ( (p_x >= 0 && p_x < WidthBlock)
+            && (p_y >= 0 && p_y < HeightBlock) )
+        {
+            return ElementArray[p_x, p_y].IsMine;
+        }
+        return false;
+    }
+
+    public int NumberOfMines(int p_x, int p_y)   //지뢰 갯수 세기
+    {
+        int outcount = 0;
+
+        //상단
+        if (GetMineAt(p_x - 1, p_y + 1)) { ++outcount; }
+        if (GetMineAt(p_x, p_y + 1)) { ++outcount; }
+        if (GetMineAt(p_x + 1, p_y + 1)) { ++outcount; }
+        //중단
+        if (GetMineAt(p_x - 1, p_y)) { ++outcount; }
+        if (GetMineAt(p_x + 1, p_y)) { ++outcount; }
+        //하단
+        if (GetMineAt(p_x - 1, p_y - 1)) { ++outcount; }
+        if (GetMineAt(p_x, p_y - 1)) { ++outcount; }
+        if (GetMineAt(p_x + 1, p_y - 1)) { ++outcount; }
+
+        return outcount;
+    }
+
+    void Start()
+    {
+        TileGenarator();
+    }
+
+    void Update()
+    {
+        
+    }
+}
--- Minesweeper/Assets/Scripts/Tile.cs
+++ Minesweeper/Assets/Scripts/Tile.cs
@@ -5,9 +5,51 @@ using UnityEngine;
 public class Tile : MonoBehaviour
 {
     // Start is called before the first frame update
+
+    public Sprite[] ChangeSpriteArray;
+    private SpriteRenderer m_SpriteRender;
+    protected GridManager LinkGridManager;
+
+    //지뢰 관련>>
+    [SerializeField]
+    private bool m_IsMine = false;
+    //public bool IsMine { get => m_IsMine; protected set => m_IsMine = value; }
+    public bool IsMine
+    {
+        get { return m_IsMine; }
+        protected set { m_IsMine = value; }
+    }
+    public void SetElementDatas(bool p_ismine)
+    {
+        m_IsMine = p_ismine;
+    }
+    //지뢰 관련<<
+
+    void SetChangeTexture(int P_index)   //그림 바꾸기
+    {
+        m_SpriteRender.sprite = ChangeSpriteArray[P_index];
+    }
+
+    void OnMouseDown()   //클릭 후 작동
+    {
+
+        if (m_IsMine)   //게임오버
+        {
+            
+        }
+        else   //지뢰 갯수 찾기
+        {
+            int x = (int)this.transform.localPosition.x;
+            int y = (int)this.transform.localPosition.y;
+            SetChangeTexture( LinkGridManager.NumberOfMines(x, y) );
+            //Debug.LogFormat("{0}", LinkGridManager.NumberOfMines(x, y));  //확인용
+        }
+    }
+
     void Start()
     {
-        
+        m_SpriteRender = this.GetComponent<SpriteRenderer>();
+        LinkGridManager = GameObject.FindObjectOfType<GridManager>();
     }
 
     // Update is called once per frame
